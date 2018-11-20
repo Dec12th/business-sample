49058b43ac1f8dd6b7688d777b572000fcdfc479
Baily <yinbah@163.com>
2018-11-20 22:57:52 +0800

    修复bug

--- business/src/main/java/com/benny/archetype/business/BusinessStarter.java
+++ business/src/main/java/com/benny/archetype/business/BusinessStarter.java
@@ -1,5 +1,7 @@
 package com.benny.archetype.business;
 
+import com.benny.archetype.common.net.annotations.CommonNetApiScan;
+import com.benny.archetype.common.net.annotations.EnableCommonNetClient;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,6 +13,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  */
 @EnableDiscoveryClient
 @SpringBootApplication
+@EnableCommonNetClient
+@CommonNetApiScan(basePackages = "com.benny.archetype.business.api.*")
 public class BusinessStarter {
     public static void main(String[] args) {
         SpringApplication springApplication = new SpringApplication(BusinessStarter.class);
